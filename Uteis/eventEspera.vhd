06f3a9ddbe1105a657dae3f9e947617876527b70
Andreas <andreasp.gomes@gmail.com>
2018-10-03 18:51:22 -0300

    Espera separada entre espera e eventEspera.

--- Uteis/eventEspera.vhd
+++ Uteis/eventEspera.vhd
@@ -1,10 +1,11 @@
 -------------------------------
--- 	Faz uma clock na frequência desejada, em ms.
+-- Gera um pulso de 1 clock depois do tempo em ms.
+-- Só conferir se saida_eventEspera = '1' para utilizar.
 ----------------------------------
 LIBRARY ieee;
 USE ieee.std_logic_1164.all;
 ----------------------------------
-ENTITY espera IS
+ENTITY eventEspera IS
 	-- ms é quantos ms você quer esperar
 	GENERIC(ms: INTEGER := 100;
 			  periodo: INTEGER := 50000
@@ -12,10 +13,10 @@ ENTITY espera IS
 	
 	PORT (clk: IN STD_LOGIC;
 		  enable: IN STD_LOGIC;
-		  saida_espera: OUT STD_LOGIC);
+			saida_eventEspera: OUT STD_LOGIC);
 END ENTITY;
 ----------------------------------
-ARCHITECTURE espera OF espera IS
+ARCHITECTURE eventEspera OF eventEspera IS
 	SIGNAL sig: STD_LOGIC;
 BEGIN
 	PROCESS(clk)
@@ -25,9 +26,9 @@ BEGIN
 		IF (rising_edge(clk) AND enable='1')  THEN
 			counter := counter + 1;
 			IF counter = (integer(ms*periodo/2)-1) THEN
-				saida_espera <= 1
+				saida_eventEspera <= 1
 			ELSE
-				saida_espera <= 0
+				saida_eventEspera <= 0
 			END IF;
 		END IF;	
 	END PROCESS;
