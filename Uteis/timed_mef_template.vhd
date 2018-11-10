75b0df4caed3a26a4f2cd4e1c2bbd781e087ab0f
Andreas <andreasp.gomes@gmail.com>
2018-11-10 11:16:15 -0200

    Template de timed state machines adicionado. Diretório da atividade 11 criado.

---
+++ Uteis/timed_mef_template.vhd
@@ -0,0 +1,33 @@
+-------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+-------------------------------------------------------------
+ENTITY <entity_name> IS
+	(same as for regular machines)
+END ENTITY;
+-------------------------------------------------------------
+ARCHITECTURE <archit_name> OF <entity_name> IS
+
+	--FSM-related declarations:
+	(same as for regular machines)
+
+	--Timer-related declarations:
+	CONSTANT T1: NATURAL := <value>;
+	CONSTANT T2: NATURAL := <value>; ...
+	CONSTANT tmax: NATURRAL := <value>;
+	SIGNAL t: NATURAL RANGE 0 TO tmax;
+
+BEGIN
+	--Timer:
+	PROCESS (clk, rst)
+	BEGIN
+		IF(rst = '1') THEN
+			t <= 0;
+	ELSIF (clk'EVENT and clk = '1') THEN
+		IF pr_state /= nx_state THEN
+		t <= 0;
+		ELSIF t /= tmax THEN
+			t <= t + 1;
+		ENF IF;
+	END IF;
+END PROCESS;
