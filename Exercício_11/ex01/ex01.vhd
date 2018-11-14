3db5aaf0de7d8e6946727845e7f046cddeed1c06
cordeirolibel <ssad@dsfjbk.jbds>
2018-11-14 18:55:20 -0200

    save

--- Exercício_11/ex01/ex01.vhd
+++ Exercício_11/ex01/ex01.vhd
@@ -4,9 +4,9 @@ USE ieee.std_logic_1164.all;
 -------------------------------------------------------------
 ENTITY ex01 IS
 	generic (
-	TAM_MAX: integer := 5; -- tamanho do balde
+	TAM_MAX: integer := 7; -- tamanho do balde
 	TEMPO: integer := 25000000; --tempo para a animacao (0.5seg)
-	DATA_WIDTH: integer := 8;  --fifo
+	DATA_WIDTH: integer := 2;  --fifo
 	FIFO_DEPTH: integer := 8);
 	port (
 		clk, rst: in std_logic;
@@ -23,19 +23,21 @@ ARCHITECTURE ex01 OF ex01 IS
 
 	--Timer-related declarations:
 	CONSTANT T1: NATURAL := TEMPO;
-	CONSTANT tmax: NATURAL := 100000000;
+	CONSTANT tmax: NATURAL := 1000000000;
+										 
 	SIGNAL t: NATURAL RANGE 0 TO tmax;
 	
-	signal s_bt_mais, s_bt_menos: std_logic;
-	signal writeEn	: STD_LOGIC;
+	signal s_bt_mais : std_logic;
+	signal s_bt_menos: std_logic;
+	signal writeEn	: STD_LOGIC := '0';
 	signal dataIn	: STD_LOGIC_VECTOR (DATA_WIDTH - 1 downto 0);
-	signal readEn	: STD_LOGIC;
+	signal readEn	: STD_LOGIC := '0';
 	signal dataOut	: STD_LOGIC_VECTOR (DATA_WIDTH - 1 downto 0);
 	signal empty: STD_LOGIC;
 	signal full	: STD_LOGIC;
 	
 	signal anim_mais, anim_menos : STD_LOGIC;
-	signal pop: STD_LOGIC_VECTOR (DATA_WIDTH - 1 downto 0);
+	signal pop: STD_LOGIC_VECTOR (DATA_WIDTH - 1 downto 0) := "00";
 BEGIN
 
 	eb1: entity work.eventButton port map (b_in => bt_mais, b_out => s_bt_mais,clk => clk);
@@ -43,8 +45,8 @@ BEGIN
 
 	ff1: entity work.fifo generic map(DATA_WIDTH=>DATA_WIDTH,FIFO_DEPTH=>FIFO_DEPTH)
 							port map (	writeEn => WriteEn, dataIn => DataIn, readEn=> ReadEn,
-								 	dataOut => DataOut, empty => Empty, full => Full,
-							 		rst => rst,clk => clk);
+											dataOut => DataOut, empty => Empty, full => Full,
+											rst => rst,clk => clk);
 
 	--Timer:
 	PROCESS (clk, rst)
@@ -67,53 +69,86 @@ BEGIN
 	--FSM combinational logic:
 	process (all) --see Note 2 above on "all" keyword
 		begin
-		case pr_state is 
-		when VAZIO =>
-			--outputs
-			anim_mais  <= '0';
-			anim_menos <= '0';
-			--
-			if pop = '+' then
-				nx_state <= ANIMA_MAIS;
-				pop <= ' ';
-			else 
-				nx_state <= VAZIO;
-			end if;
-		when ANIMA_MAIS =>
-			anim_mais  <= '1';
-			anim_menos <= '0';
-			--...
-			if t >= 100000000 then
-				nx_state <= VAZIO;
-			else
-				nx_state <= ANIMA_MAIS;
-			end if;
-		end case;
+		IF(rst = '0') THEN
+			readEn <= '0';
+			pop <= "00";-- '00' = null
+			nx_state<=VAZIO;
+		ELSE
+			--ler FIFO 
+			IF((empty = '0') and (writeEn = '0') and (pop = "00")) THEN --  "00" - null
+				readEn <= '1';
+				pop <= DataOut;
+			ELSE
+				readEn <= '0';
+			END IF;
+			
+			-- Proximos estados
+			case pr_state is 
+			when VAZIO =>
+				--outputs
+				anim_mais  <= '1';
+				anim_menos <= '1';
+				--
+				if pop = "01" then -- '01' = mais
+					nx_state <= ANIMA_MAIS;
+					pop <= "00";-- '00' = null
+				elsif pop = "11" then -- '11' = menos
+					nx_state <= ANIMA_MENOS;
+					pop <= "00";-- '00' = null
+				else 
+					nx_state <= VAZIO;
+				end if;
+			when ANIMA_MAIS =>
+				anim_mais  <= '1';
+				anim_menos <= '0';
+				--...
+				if t >= 100000000 then
+					nx_state <= VAZIO;
+				else
+					nx_state <= ANIMA_MAIS;
+				end if;
+			when ANIMA_MENOS =>
+				anim_mais  <= '0';
+				anim_menos <= '1';
+				--...   
+				if t >= 100000000 then
+					nx_state <= VAZIO;
+				else
+					nx_state <= ANIMA_MENOS;
+				end if;
+			when others => 
+				anim_mais  <= '0';
+				anim_menos <= '0';
+			end case;
+		END IF;
 	end process;
 
 	--botoes para FIFO 
 	PROCESS (clk)
 	BEGIN
-		IF(s_bt_menos = '1') AND(full = '0') THEN
-			writeEn <= '1';
-			dataIn <= '-';
-		ELSIF (s_bt_mais = '1') AND(full = '0') THEN
-			writeEn <= '1';
-			dataIn <= '+';
-		ELSE 
+		IF(rst = '0') THEN
 			writeEn <= '0';
+		ELSE
+			IF(s_bt_menos = '1') THEN -- AND(full = '0')
+				dataIn <= "11";--menos
+				writeEn <= '1';
+			ELSIF (s_bt_mais = '1') THEN-- AND(full = '0')
+				dataIn <= "01";--mais
+				writeEn <= '1';
+			ELSE 
+				dataIn <= "00";
+				writeEn <= '0';
+			END IF;
 		END IF;
 	END PROCESS;
 
-	--ler FIFO 
-	PROCESS (clk)
-	BEGIN
-		IF(empty = '0' and pop = ' ') THEN
-			readEn <= '1';
-			pop <= DataIn;
-		END IF;
-	END PROCESS;
-
-	led_rst(0) <= anim_mais;
+	leds(1) <= anim_menos;
+	leds(0) <= anim_mais;
+	
+	leds(2) <= empty;
+	leds(3) <= pop(0);
+	leds(4) <= pop(1);
+	leds(5) <= dataOut(0);
+	leds(6) <= dataOut(1);
 end architecture;
 -------------------------------------------------------------  
